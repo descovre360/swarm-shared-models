cf9a51cbbac326eb701f8c97ccbc07832c315c2a
Swaroop Murthy <smurthy@inno-360.com>
2017-06-30 17:14:12 +0530

    lose devise from baseclass

--- lib/swarm_shared_models/user.rb
+++ lib/swarm_shared_models/user.rb
@@ -2,9 +2,9 @@ module SwarmSharedModels
   class User < ApplicationRecord
     # Include default devise modules. Others available are:
     # :confirmable, :lockable, :timeoutable and :omniauthable
-    devise :database_authenticatable, :registerable,
-           :recoverable, :rememberable, :trackable,
-           :validatable, :confirmable, :lockable
+    # devise :database_authenticatable, :registerable,
+    #        :recoverable, :rememberable, :trackable,
+    #        :validatable, :confirmable, :lockable
 
     belongs_to :tenant
     validates_presence_of :first_name
