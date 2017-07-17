83cc7e38a849b0b5fdc76f2468afbcf2bc6d4f0b
Swaroop Murthy <smurthy@inno-360.com>
2017-07-17 15:28:46 -0400

    expose through namespace

--- lib/swarm_shared_models.rb
+++ lib/swarm_shared_models.rb
@@ -2,5 +2,13 @@ module SwarmSharedModels
   VERSION = "0.1.0"
   # Your code goes here...
   autoload :Tenant, 'swarm_shared_models/tenant'
-  autoload :User, 'swarm_shared_models/user'
+  autoload :History, 'swarm_shared_models/history'
+  autoload :Preference, 'swarm_shared_models/preference'
+  autoload :Permission, 'swarm_shared_models/permission'
+  autoload :ReleaseNote, 'swarm_shared_models/release_note'
+  autoload :Role, 'swarm_shared_models/role'
+  autoload :SourceGroup, 'swarm_shared_models/source_group'
+  autoload :Source, 'swarm_shared_models/source'
+  autoload :VisualizationGroup, 'swarm_shared_models/visualization_group'
+  autoload :Visualization, 'swarm_shared_models/visualization'
 end
