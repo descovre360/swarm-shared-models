84490a8289c7c35d4b49f0b5bf14468a5d46f091
Swaroop Murthy <smurthy@inno-360.com>
2017-08-02 10:39:49 -0400

    new validation rules

--- lib/swarm_shared_models/dashboard_pane.rb
+++ lib/swarm_shared_models/dashboard_pane.rb
@@ -3,6 +3,11 @@ module SwarmSharedModels
     belongs_to :user
     belongs_to :visualization
     belongs_to :folder
+    belongs_to :feed_source
     delegate :tenant, to: :user
+
+    validates :folder, presence: true, if: Proc.new { |a| a.feed_source_id.blank? }
+    validates :visualization, presence: true, if: Proc.new { |a| a.feed_source_id.blank? }
+    validates :feed_source, presence: true, if: Proc.new { |a| a.folder_id.blank? && a.visualization_id.blank? }
   end
 end
