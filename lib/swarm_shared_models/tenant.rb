50ca397ef90252ce950ac4652ff3d0afb825da61
chmadhu13 <chmadhu13@gmail.com>
2017-10-04 22:07:13 -0500

    Added owner method in tenant

--- lib/swarm_shared_models/tenant.rb
+++ lib/swarm_shared_models/tenant.rb
@@ -2,5 +2,11 @@ module SwarmSharedModels
   class Tenant < ApplicationRecord
     has_many :users
     has_many :user_visits, through: :users
+
+    def owner
+      owner_user_role = UserRole.where(role_id: Role.where(name: 'Owner').first.id, user_id: users.pluck(:id)).first
+      owner_user_role && owner_user_role.user
+    end
+
   end
 end
