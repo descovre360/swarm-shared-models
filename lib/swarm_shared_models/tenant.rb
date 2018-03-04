96624243be550c07485a7a67e112c33095ee2605
chmadhu13 <chmadhu13@gmail.com>
2018-03-04 12:00:27 -0600

    Method to give sorted users for a tenant based on args

--- lib/swarm_shared_models/tenant.rb
+++ lib/swarm_shared_models/tenant.rb
@@ -12,5 +12,16 @@ module SwarmSharedModels
       UserRole.where(role_id: Role.where(name: 'Owner').first.id, user_id: users.pluck(:id)).includes(:user).map{|user_role| user_role.user}
     end
 
+    def sorted_users(sort_field_order = nil)
+      sort_field_order = 'first_name_asc' if sort_field_order.blank?
+      sort_type = sort_field_order.split('_').last
+      sort_by = sort_field_order.split('_')[0..-2].join('_')
+      unless sort_by == 'invite'
+        ::User.where(tenant_id: id).order("#{sort_by} #{sort_type}")
+      else
+        ::User.where(tenant_id: id)
+      end
+    end
+
   end
 end
