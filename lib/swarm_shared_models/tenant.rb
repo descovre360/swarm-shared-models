1c3c95abf5c23167dc096c1702bea7bddd1fb380
chmadhu13 <chmadhu13@gmail.com>
2018-04-21 12:03:52 -0500

    Nested attrs support for tenant

--- lib/swarm_shared_models/tenant.rb
+++ lib/swarm_shared_models/tenant.rb
@@ -3,6 +3,8 @@ module SwarmSharedModels
     has_many :users
     has_many :user_visits, through: :users
 
+    accepts_nested_attributes_for :users
+
     def owner
       owner_user_role = UserRole.where(role_id: Role.where(name: 'Owner').first.id, user_id: users.pluck(:id)).first
       owner_user_role && owner_user_role.user
